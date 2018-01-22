88008584cb5711741ee04c49eed84c883161b0a0
Syara <kamura.syara@gmail.com>
2018-01-23 00:54:46 +0900

    Syara
    SaveDataの有無と削除を行う関数を作成

---
+++ MMORPG-Provisional   Ver1.22/Code/AppData/SaveData/SaveData.cpp
@@ -0,0 +1,55 @@
+﻿/*
+				ファイル名		:SaveData.cpp
+				作成者			:Syara
+				作成日時		:2018/01/23
+				ソース説明		:
+					
+					セーブデータ
+				
+				備考
+				
+				
+				
+!*/
+#include "DxLib.h"
+#include "SaveData.h"
+#include "../Code/Common/FileIO/WriteBynary.h"
+#include "../Code/Common/FileIO/ReadBynary.h"
+
+/*
+	セーブデータが存在しているか判断する
+	return	true	:存在している
+			false	:存在していない
+*/
+bool SaveData_Exits(const char* fileName, ...) {
+
+	char path[256];
+
+	va_list vaList;
+
+	va_start(vaList, fileName);
+	vsprintfDx(path, fileName, vaList);
+	va_end(vaList);
+	
+	ReadBynary file;
+	return file.Exist(path);
+}
+
+/*
+	ファイルの削除を行う
+	return	true	:削除成功
+			false	:削除失敗
+*/
+bool SaveData_Remove(const char* fileName, ...) {
+
+	char path[256];
+
+	va_list vaList;
+
+	va_start(vaList, fileName);
+	vsprintfDx(path, fileName, vaList);
+	va_end(vaList);
+
+	ReadBynary file;
+	return file.Remove(path);
+}
---
+++ MMORPG-Provisional   Ver1.22/Code/AppData/SaveData/SaveData.h
@@ -0,0 +1,32 @@
+﻿/*
+				ファイル名		:SaveData.h
+				作成者			:
+				作成日時		:2018/01/23
+				ソース説明		:
+					
+					セーブデータ
+				
+				備考
+				
+				
+				
+!*/
+
+#ifndef __SAVE_DATA_H__
+#define __SAVE_DATA_H__
+
+/*
+	セーブデータが存在しているか判断する
+	return	true	:存在している
+			false	:存在していない
+*/
+bool SaveData_Exits(const char* fileName, ...);
+
+/*
+	ファイルの削除を行う
+	return	true	:削除成功
+			false	:削除失敗
+*/
+bool SaveData_Remove(const char* fileName, ...);
+
+#endif // __SAVE_DATA_H__
