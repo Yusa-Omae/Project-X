7841708561344292d421fb0d9a6ab520493d8017
Syara <kamura.syara@gmail.com>
2018-01-23 22:58:30 +0900

    Syara
    ゲームメインにステートを追加しました

--- MMORPG-Provisional   Ver1.22/Gamemain.cpp
+++ MMORPG-Provisional   Ver1.22/Gamemain.cpp
@@ -12,7 +12,169 @@
 #include"Stage.h"
 #include"Chara.h"
 
+#include "Code/AppData/SaveData/SaveData.h"
 
+#define SAVE_DATA_FILENAME ("saveData.sav")
+#define FADE_TIME (60)
+
+
+
+typedef struct {
+	eGameState nowGameState;	//今のゲームの状態
+	eGameState nextGameState;	//次のゲームの状態
+	eFadeType fadeType;
+	float alaph;				//アルファブレンド
+	unsigned int fadeColor;		//フェード時の色
+	int fadeCounter;			//フェード用カウンター
+}WORK_OBJ_t;
+
+static WORK_OBJ_t s_Work;
+
+
+static bool FadeIn_Proc() {
+
+	if (s_Work.fadeCounter >= FADE_TIME && s_Work.alaph < 0.0f) {
+		s_Work.alaph = 0.0f;
+		return false;
+	}
+
+	s_Work.alaph = 255.0f - 255.0f * (float)s_Work.fadeCounter / (float)FADE_TIME;
+
+	return true;
+}
+
+static bool FadeOut_Proc() {
+
+	if (s_Work.fadeCounter >= FADE_TIME && s_Work.alaph > 255.0f) {
+		s_Work.alaph = 255.0f;
+		return false;
+	}
+
+	s_Work.alaph = 255.0f * (float)s_Work.fadeCounter / (float)FADE_TIME;
+
+	return true;
+
+}
+
+static bool FadeCross_Proc() {
+
+	if (s_Work.fadeCounter >= FADE_TIME && s_Work.alaph < 0.0f) {
+		s_Work.alaph = 0.0f;
+		return false;
+	}
+
+	float fadeTime = (float)FADE_TIME / 2.0f;
+
+	if (s_Work.fadeCounter < fadeTime) {
+
+		s_Work.alaph = 255.0f * (float)s_Work.fadeCounter / fadeTime;
+	}
+	else {
+		s_Work.alaph = 255.0f - 255.0f * (float)(s_Work.fadeCounter - fadeTime) / fadeTime;
+	}
+	return true;
+}
+
+/*
+	フェード
+	return	true	:フェード中
+			false	:フェード終了
+*/
+static bool Fade_Proc(int fadeType) {
+
+	//フェード終了中
+	if (fadeType == eFadeType_None) return false;
+	
+	//フェード中であるか？
+	bool isFadePlay = true;
+
+
+	if (fadeType == eFadeType_In) {
+		isFadePlay = FadeIn_Proc();
+	}
+	else if (fadeType == eFadeType_Out) {
+		isFadePlay = FadeOut_Proc();
+	}
+	else {
+		isFadePlay = FadeCross_Proc();
+	}
+
+	s_Work.fadeCounter++;
+
+	return isFadePlay;
+}
+
+static void Fade_Draw() {
+
+	int alpha = s_Work.alaph;
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA,alpha);
+	DrawBox(0, 0, INIT_AREA_X2, INIT_AREA_Y2,s_Work.fadeColor,TRUE);
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+}
+
+/*
+	ゲームメイン更新
+*/
+static void GameMain_Proc() {
+	//カメラの更新
+	CameraUpdate();
+
+	PlayerUpdate();
+	Chara_Update();
+}
+
+/*
+	ゲームメイン描画
+*/
+static void GameMain_Draw() {
+	//カメラセット
+	CameraSetUp();
+	StageDraw();
+	DrawCube3D(VSub(Get_Player_Pos(), VGet(25.0, 25.0, 25.0)), VAdd(Get_Player_Pos(), VGet(25.0, 25.0, 25.0)), Color_ValTbl[eCol_EmeraldGreen], 0, 1);
+	Chara_Draw();
+	Debug_Player();
+	Debug_Camera();
+	Debag_Chara();
+
+
+}
+
+/*
+	ゲームメインのステートを切り替える
+	eGameState gameState				:次のゲームステートを設定
+	eFadeType fadeType = eFadeType_None	:フェードのタイプを設定　基本はフェードしない状態になっている
+	
+		ゲームステート
+		eGameState_None,		//状態なし
+		eGameState_Initialize,	//初期化
+		eGameState_Fade,		//フェード		※これを次のステートに設定するとシーンが切り替わらないくなります
+		eGameState_Tutorial,	//チュートリアル
+		eGameState_MainGame,	//メインゲーム
+		eGameState_Pouse,		//ポーズ画面
+		eGameState_Shop,		//ショップ
+
+		フェードタイプ
+		eFadeType_None,			//状態なし
+		eFadeType_In,			//フェードイン
+		eFadeType_Out,			//フェードアウト
+		eFadeType_CrossFade,	//クロスフェード
+
+*/
+void GameMain_ChangeGameState(eGameState gameState, eFadeType fadeType/* = eFadeType_None*/) {
+
+	if (fadeType == eFadeType_None) {
+		s_Work.nowGameState = gameState;
+	}
+	else {
+		s_Work.nowGameState = eGameState_Fade;
+		s_Work.nextGameState = gameState;
+		s_Work.fadeType = fadeType;
+
+	}
+
+
+}
 
 
 void GameMainInitialize(){
@@ -22,29 +184,88 @@ void GameMainInitialize(){
 	Camera_Initialize();
 
 	Chara_Initialize();
+
+	s_Work.nowGameState = eGameState_Initialize;
+
+	if (SaveData_Exits(SAVE_DATA_FILENAME) == true) {
+		s_Work.nextGameState = eGameState_MainGame;
+	}
+	else {
+		s_Work.nextGameState = eGameState_Tutorial;
+	}
+
+	s_Work.fadeCounter = 0;
+	s_Work.alaph = 0.0f;
+	s_Work.fadeColor = GetColor(255, 255, 255);
+
+
 }
 void GameMainupdate(){
 
-	//カメラの更新
-	CameraUpdate();
+	eGameState state = s_Work.nowGameState;
 
-	PlayerUpdate();
-	Chara_Update();
+	switch (state) {
+	case eGameState_Initialize:
+
+		s_Work.nowGameState = eGameState_Fade;
+		s_Work.fadeType = eFadeType_In;
+		break;
+	case eGameState_Fade:
+		if (Fade_Proc(s_Work.fadeType) == false) {
+			s_Work.nowGameState = s_Work.nextGameState;
+			s_Work.nextGameState = eGameState_None;
+			s_Work.fadeCounter = 0;
+		}
+
+		break;
+	case eGameState_Tutorial:
+
+		//今はないのでゲームメインに移行
+		s_Work.nowGameState = eGameState_MainGame;
+		break;
+	case eGameState_MainGame:
+		GameMain_Proc();
+		break;
+	case eGameState_Pouse:
+
+		break;
+	case eGameState_Shop:
+
+		break;
+	}
+
+
 
 
 }
 void GameMainDraw(){
 	
-	//カメラセット
-	CameraSetUp();
-	StageDraw();
-	DrawCube3D(VSub(Get_Player_Pos(),VGet(25.0,25.0,25.0)),VAdd(Get_Player_Pos(),VGet(25.0,25.0,25.0)),Color_ValTbl[eCol_EmeraldGreen],0,1);
-	Chara_Draw();
-	Debug_Player();
-	Debug_Camera();
-	Debag_Chara();
+	eGameState state = s_Work.nowGameState;
 
+	switch (state) {
+	case eGameState_Initialize:
+
+		break;
+	case eGameState_Fade:
+		Fade_Draw();
+		break;
+	case eGameState_Tutorial:
+
+		break;
+	case eGameState_MainGame:
+		GameMain_Draw();
+		break;
+	case eGameState_Pouse:
+
+		break;
+	case eGameState_Shop:
+
+		break;
+	}
+
+	
 }
+
 void GameMainFainalize(){
 
 	Player_Info_Fainalize();
--- MMORPG-Provisional   Ver1.22/Gamemain.h
+++ MMORPG-Provisional   Ver1.22/Gamemain.h
@@ -1,6 +1,55 @@
-#pragma once
+
+
+#ifndef __GAME_MAIN_H__
+#define __GAME_MAIN_H__
+
+
+enum eFadeType {
+	eFadeType_None = -1,	//状態なし
+	eFadeType_In,			//フェードイン
+	eFadeType_Out,			//フェードアウト
+	eFadeType_CrossFade,	//クロスフェード
+};
+
+enum eGameState {
+	eGameState_None = -1,	//状態なし
+	eGameState_Initialize,	//初期化
+	eGameState_Fade,		//フェード
+	eGameState_Tutorial,	//チュートリアル
+	eGameState_MainGame,	//メインゲーム
+	eGameState_Pouse,		//ポーズ画面
+	eGameState_Shop,		//ショップ
+};
+
 
 void GameMainInitialize();
 void GameMainupdate();
 void GameMainDraw();
 void GameMainFainalize();
+
+
+/*
+ゲームメインのステートを切り替える
+eGameState gameState				:次のゲームステートを設定
+eFadeType fadeType = eFadeType_None	:フェードのタイプを設定　基本はフェードしない状態になっている
+
+ゲームステート
+eGameState_None,		//状態なし
+eGameState_Initialize,	//初期化
+eGameState_Fade,		//フェード		※これを次のステートに設定するとシーンが切り替わらないくなります
+eGameState_Tutorial,	//チュートリアル
+eGameState_MainGame,	//メインゲーム
+eGameState_Pouse,		//ポーズ画面
+eGameState_Shop,		//ショップ
+
+フェードタイプ
+eFadeType_None,			//状態なし
+eFadeType_In,			//フェードイン
+eFadeType_Out,			//フェードアウト
+eFadeType_CrossFade,	//クロスフェード
+
+*/
+void GameMain_ChangeGameState(eGameState gameState, eFadeType fadeType = eFadeType_None);
+
+
+#endif	//__GAME_MAIN_H__
