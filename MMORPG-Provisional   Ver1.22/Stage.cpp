61dc81edd5009b75e04ca38bf77a61772ea37eab
Yusa-Omae <rain0328@icloud.com>
2018-01-24 00:38:17 +0900

    ステージ処理(おおまかに)
    ステージの読み込み、描画
    影の設定、描画に対応しました。
    各種オブジェクトの設定も終わっていて
    コメントアウトを外せば描画できるようになっています。
    あ、複数ある場合を考えてなかったので後で追加でハンドルと別に描画用のオブジェクトハンドル作っておきます。

--- MMORPG-Provisional   Ver1.22/Stage.cpp
+++ MMORPG-Provisional   Ver1.22/Stage.cpp
@@ -5,9 +5,35 @@
 #include"math.h"
 #include"stdlib.h"
 #include"Chara_Player.h"
+#include"Stage.h"
+
+s_Stage STAGE;
+
 
 //ステージクラスの初期化処理
 void StageInitialize(){
+	
+	//モデルの読み込み
+	STAGE.handle = MV1LoadModel("Stage\\Stage00.mv1");
+
+	/*
+	for(int i = 0;i < 100;i++){
+
+		STAGE.obj[i] = MV1LoadModel("Stage\\Stage_Obj%03d.mv1",i);
+		STAGE.c_obj[i] = MV1LoadModel("Stage\\Stage_Obj%03d_c.mv1",i);
+
+	}
+	*/
+
+	//シャドウマップの作成
+	STAGE.smap = MakeShadowMap(1024, 1024);
+	SetShadowMapLightDirection(STAGE.smap, VGet(1,-1,0));				//カメラのライトと合わせるときにライティングのベクトルをここにもいれるとOK
+	SetShadowMapDrawArea(STAGE.smap, SHADOWMAP_INIT1, SHADOWMAP_INIT2);
+
+
+	//位置設定
+	MV1SetPosition(STAGE.handle, STAGE_INIT);
+
 
 
 }
@@ -15,13 +41,38 @@ void StageInitialize(){
 //ステージクラスの更新処理
 void StageUpdata(){
 
+	
+
+
+
 }
 
 //ステージクラスの描画処理
 void StageDraw(){
 
-	DrawCube3D(VGet(-5000,0,-5000),VGet(5000,-50,5000),GetColor(255,255,255),GetColor(0,255,127),0);
-	DrawCube3D(VGet(-5000,0,-5000),VGet(5000,-50,5000),GetColor(255,255,255),GetColor(125,125,125),1);
+	//シャドウマップに描きこみ
+	ShadowMap_DrawSetup(STAGE.smap);
+	MV1DrawModel(STAGE.handle);
+	/*
+	for(int i = 0;i < 100;i++){
+
+	MV1DrawModel(STAGE.obj[i]);
+
+	}
+	*/
+	ShadowMap_DrawEnd();
+	SetUseShadowMap(0, STAGE.smap);
+
+	//モデル描画
+	MV1DrawModel(STAGE.handle);
+	/*
+	for(int i = 0;i < 100;i++){
+
+	MV1DrawModel(STAGE.obj[i]);
+
+	}
+	*/
+	SetUseShadowMap(0,-1);
 
 }
 
--- MMORPG-Provisional   Ver1.22/Stage.h
+++ MMORPG-Provisional   Ver1.22/Stage.h
@@ -4,3 +4,23 @@ void StageInitialize();
 void StageUpdata();
 void StageDraw();
 void StageFainalize();
+
+//ステージクラスで使用する構造体
+typedef struct s_Stage {
+
+	//ステージデータ格納ハンドル
+	int handle;
+
+	//オブジェクト格納ハンドルは仮で100個配列をつくっています。
+	//オブジェクトデータ格納ハンドル
+	int obj[100];
+
+	//あたり判定用オブジェクトデータ格納ハンドル
+	int c_obj[100];
+
+
+	//シャドウマップの作成用ハンドル
+	int smap;
+
+
+};
